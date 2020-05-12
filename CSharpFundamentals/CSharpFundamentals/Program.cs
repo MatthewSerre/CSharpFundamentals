2b100defdbfd12c107bdd6ec1ea9c486b135fd22
Matthew Serre <matthew.serre@gmail.com>
2020-05-11 20:38:51 -0400

    Practice using enums

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -5,10 +5,27 @@ using System.Runtime.InteropServices;
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+
             //var john = new Person();
             //john.FirstName = "John";
             //john.LastName = "Smith";
@@ -34,17 +51,17 @@ namespace CSharpFundamentals
 
             //var names = string[3] { "Jack", "John", "Mary"};
 
-            var firstName = "Matthew";
-            var lastName = "Serre";
-            var name = firstName + " " + lastName;
-            var fullName = string.Format("My name is {0} {1}.", firstName, lastName);
-            var names = new string[3] { "John", "Mary", "Jack" };
-            var formattedNames = string.Join(", ", names);
-            Console.WriteLine(formattedNames);
-            var text = @"Hi John
-Look into the following paths
-\c:\user\Matthew";
-            Console.WriteLine(text);
+//            var firstName = "Matthew";
+//            var lastName = "Serre";
+//            var name = firstName + " " + lastName;
+//            var fullName = string.Format("My name is {0} {1}.", firstName, lastName);
+//            var names = new string[3] { "John", "Mary", "Jack" };
+//            var formattedNames = string.Join(", ", names);
+//            Console.WriteLine(formattedNames);
+//            var text = @"Hi John
+//Look into the following paths
+//\c:\user\Matthew";
+//            Console.WriteLine(text);
         }
     }
 }
